9c0ee02323df092ff90544ab8a089f2a08cd0171
mdzahidhaider <mdzahidhaider@gmail.com>
2023-03-29 13:00:07 +0530

    exception

--- Sweetopia-Server/src/main/java/com/sweetopia/exception/UserNotFoundException.java
+++ Sweetopia-Server/src/main/java/com/sweetopia/exception/UserNotFoundException.java
@@ -1,12 +1,12 @@
 package com.sweetopia.exception;
 
-public class UserNotFound extends Exception{
+public class UserNotFoundException extends Exception{
 	
-	public UserNotFound() {
+	public UserNotFoundException() {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public UserNotFound(String message) {
+	public UserNotFoundException(String message) {
 		// TODO Auto-generated constructor stub
 		super();
 	}
