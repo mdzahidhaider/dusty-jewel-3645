2f8667ac163f8ba69669500d0cbd95b072887329
zoneAnaS <shaikhanas4321@gmail.com>
2023-03-29 09:44:19 +0530

    Created Product table

--- Sweetopia-Server/src/main/java/com/sweetopia/entity/Product.java
+++ Sweetopia-Server/src/main/java/com/sweetopia/entity/Product.java
@@ -1,5 +1,45 @@
 package com.sweetopia.entity;
 
-public class Product {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+    @NotNull
+    private String name;
+    @NotNull
+    private String photoPath;
+    @NotNull
+    private Double price;
+    @NotNull
+    private String description;
+    @NotNull
+    @JsonIgnore
+    private Integer available;
+    @ManyToOne
+    private Category category;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return productId.equals(product.productId) && name.equals(product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productId, name);
+    }
 }
