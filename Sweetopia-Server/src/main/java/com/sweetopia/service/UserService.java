d769a8f523a48d0af90eb0a7a697b45327604483
mdzahidhaider <mdzahidhaider@gmail.com>
2023-03-30 01:05:24 +0530

    user

--- Sweetopia-Server/src/main/java/com/sweetopia/service/UserService.java
+++ Sweetopia-Server/src/main/java/com/sweetopia/service/UserService.java
@@ -3,12 +3,13 @@ package com.sweetopia.service;
 import java.util.List;
 
 import com.sweetopia.entity.User;
+import com.sweetopia.exception.UserNotFoundException;
 
 public interface UserService {
 		
 	public User addUser(User newUser);
-	public List<User> getAllUser();
-	public User updateUserDetails(User user);
+	public List<User> getAllUser() throws UserNotFoundException;
+	public User updateUserDetails(User user) throws UserNotFoundException;
 	public String deleteUser(Long id);
 	public String deleteAllUser();
 	
--- Sweetopia-Server/src/main/java/com/sweetopia/service/implementation/UserServiceImpl.java
+++ Sweetopia-Server/src/main/java/com/sweetopia/service/implementation/UserServiceImpl.java
@@ -1,11 +1,13 @@
 package com.sweetopia.service.implementation;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sweetopia.entity.User;
+import com.sweetopia.exception.UserNotFoundException;
 import com.sweetopia.repository.UserRepository;
 import com.sweetopia.service.UserService;
 
@@ -18,20 +20,30 @@ public class UserServiceImpl implements UserService{
 	@Override
 	public User addUser(User newUser) {
 		// TODO Auto-generated method stub
+		User savedUser = userRepository.save(newUser);
 		
-		return null;
+		return savedUser;
 	}
 
 	@Override
-	public List<User> getAllUser() {
+	public List<User> getAllUser() throws UserNotFoundException {
 		// TODO Auto-generated method stub
-		return null;
+		List<User> users = userRepository.findAll();
+		if(users.isEmpty()) {
+			throw new UserNotFoundException("User Not Found");
+		}else
+		return users;
 	}
 
 	@Override
-	public User updateUserDetails(User user) {
+	public User updateUserDetails(User user) throws UserNotFoundException {
 		// TODO Auto-generated method stub
-		return null;
+		Optional<User> opt = userRepository.findById(user.getId());
+		if(opt.isEmpty()) {
+			throw new UserNotFoundException();
+		}else
+			return userRepository.save(user);
+		
 	}
 
 	@Override
