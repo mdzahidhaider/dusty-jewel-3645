89846e23785a7f1493dbecf873d534dbd441428c
Kshitija Dokarmare <kshitijadokarmare786@gmail.com>
2023-03-29 15:01:14 +0530

    day1 work done

---
+++ Sweetopia-Server/src/main/java/com/sweetopia/service/implementation/CustomerServiceImpl.java
@@ -0,0 +1,40 @@
+package com.sweetopia.service.implementation;
+
+import java.util.List;
+
+import com.sweetopia.entity.Customer;
+import com.sweetopia.service.CustomerService;
+
+public class CustomerServiceImpl implements CustomerService{
+
+	@Override
+	public Customer addCustomer(Customer customer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Customer updateCustomer(Customer cudtomer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Customer cancelCustomer(int CustomerId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Customer> showAllCustomers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Customer> showAllCustomers(int CustomerId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
