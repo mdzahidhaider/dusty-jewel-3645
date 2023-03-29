78f749689cef88e6f40c53ec1e314caf68eaa96c
mdzahidhaider <mdzahidhaider@gmail.com>
2023-03-29 12:36:55 +0530

    user

---
+++ Sweetopia-Server/src/main/java/com/sweetopia/exception/UserNotFound.java
@@ -0,0 +1,15 @@
+package com.sweetopia.exception;
+
+public class UserNotFound extends Exception{
+	
+	public UserNotFound() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public UserNotFound(String message) {
+		// TODO Auto-generated constructor stub
+		super();
+	}
+	
+
+}
