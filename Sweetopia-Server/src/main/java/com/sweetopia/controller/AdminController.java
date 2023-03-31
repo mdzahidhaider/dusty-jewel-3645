e73c9102cf4308ee3e9977e09d65402deefff0fb
mdzahidhaider <mdzahidhaider@gmail.com>
2023-03-31 10:33:10 +0530

    admin

--- Sweetopia-Server/src/main/java/com/sweetopia/controller/AdminController.java
+++ Sweetopia-Server/src/main/java/com/sweetopia/controller/AdminController.java
@@ -8,9 +8,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sweetopia.entity.Category;
 import com.sweetopia.entity.Customer;
+import com.sweetopia.entity.Product;
 import com.sweetopia.entity.User;
+import com.sweetopia.exception.CategoryException;
 import com.sweetopia.exception.CustomerNotFoundException;
+import com.sweetopia.exception.ProductException;
 import com.sweetopia.exception.UserNotFoundException;
 import com.sweetopia.service.AdminService;
 
@@ -22,17 +26,32 @@ public class AdminController {
 	private AdminService adminService;
 	
 	@GetMapping("/admin/users")
-	
 	public ResponseEntity<List<User>> getListOfAllUsers() throws UserNotFoundException{
 		List<User> users = adminService.allUsers();
 		return new ResponseEntity<>(users,HttpStatus.FOUND);
 		
 		
 	}
-	
+	@GetMapping("/admin/customers")
 	public ResponseEntity<List<Customer>> getListOfAllCustomers() throws CustomerNotFoundException{
 		List<Customer> customers = adminService.allCustomers();
 		return new ResponseEntity<>(customers,HttpStatus.FOUND);
+		
+		
+	}
+	
+	@GetMapping("/admin/products")
+	public ResponseEntity<List<Product>> getListOfAllProducts() throws ProductException{
+		List<Product> products = adminService.allProducts();
+		return new ResponseEntity<>(products,HttpStatus.FOUND);
+		
+		
+	}
+	
+	@GetMapping("/admin/categories")
+	public ResponseEntity<List<Category>> getListOfAllCategories() throws CategoryException{
+		List<Category> categories = adminService.allCategories();
+		return new ResponseEntity<>(categories,HttpStatus.FOUND);
 		
 		
 	}
