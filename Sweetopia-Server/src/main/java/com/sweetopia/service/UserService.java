f8af169e6842c712780461675b699e41e17c82d8
mdzahidhaider <mdzahidhaider@gmail.com>
2023-04-02 20:16:55 +0530

    fixed

--- Sweetopia-Server/src/main/java/com/sweetopia/service/UserService.java
+++ Sweetopia-Server/src/main/java/com/sweetopia/service/UserService.java
@@ -1,10 +1,10 @@
-package com.sweetopia.service;
-
-import com.sweetopia.entity.User;
-import com.sweetopia.exception.AdminNotFoundException;
-
-public interface UserService {
-	
-	public org.springframework.security.core.userdetails.User loadUserByUsername(String username) throws AdminNotFoundException;
-	public User saveUser(User user);
-}
+//package com.sweetopia.service;
+//
+//import com.sweetopia.entity.User;
+//import com.sweetopia.exception.AdminNotFoundException;
+//
+//public interface UserService {
+//	
+//	public org.springframework.security.core.userdetails.User loadUserByUsername(String username) throws AdminNotFoundException;
+//	public User saveUser(User user);
+//}
