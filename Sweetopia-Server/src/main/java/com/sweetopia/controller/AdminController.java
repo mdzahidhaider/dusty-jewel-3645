2828f9e91bcfb9034734f51270c5a388489efa63
mdzahidhaider <mdzahidhaider@gmail.com>
2023-04-02 10:48:57 +0530

    added cross origin

--- Sweetopia-Server/src/main/java/com/sweetopia/controller/AdminController.java
+++ Sweetopia-Server/src/main/java/com/sweetopia/controller/AdminController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,7 @@ import com.sweetopia.service.AdminService;
 
 @RestController
 @RequestMapping("/sweetopia")
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 public class AdminController {
 	
 	
